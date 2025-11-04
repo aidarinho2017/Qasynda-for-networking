2e9e948c9eeac605ad4631f0265c36591d2da8ca
aidarinho <isakhanovaidar@gmail.com>
2025-11-04 20:50:56 +0500

    let's go

--- DjangoPracticeEvents/settings.py
+++ DjangoPracticeEvents/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from datetime import timedelta
+import os
 from pathlib import Path
 
 from django.contrib import staticfiles
@@ -152,7 +153,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
