860c08416a3894861580ea54ae9d3bd66e635d09
aidarinho <isakhanovaidar@gmail.com>
2025-11-04 21:17:59 +0500

    let's go334

--- DjangoPracticeEvents/settings.py
+++ DjangoPracticeEvents/settings.py
@@ -10,17 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import sys
-from datetime import timedelta
-import os
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 from django.contrib import staticfiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import os
-from dotenv import load_dotenv
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 load_dotenv()
 
 # Quick-start development settings - unsuitable for production
@@ -153,8 +155,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
